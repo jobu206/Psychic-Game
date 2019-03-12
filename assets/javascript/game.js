6bb4e276953fedeec211a376ee9013f7d163f739
KevinL <mastahkevin@gmail.com>
2019-03-12 06:49:46 -0700

    Incrementing Losses & decrementing guesses in JS. Updated HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
     // declare variables
 
     // computer options
-    var options = ["a", "b", "c", "d", "e", "f", "g"];
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     // variables for wins, losses, draws and guesses
     var wins = 0;
     var losses = 0;
-    var guesses = 5;
+    var guessesLeft = 5;
 
     // computer to pick random letter from array
     var computerChoice = options[Math.floor(Math.random()*options.length)];
@@ -18,15 +18,26 @@
         var userGuess = event.key.toLowerCase();
 
         // start comparisons
+        // if (userGuess === computerChoice) {
+        //     wins++;
+        // } else {
+        //     guesses--;
+        // } if (guesses === 0) {
+        //     losses++;
+        // }
         if (userGuess === computerChoice) {
+            // increment wins
             wins++;
         } else {
-            guesses--;
-        } if (guesses === 0) {
-            losses++;
-        } 
+            // increment losses && decrement # of guesses left.
+            losses++, guessesLeft--;
+        } if (guessesLeft === 0) {
+            // reset vars to start game again.
+            wins = 0, losses = 0, guessesLeft = 5;
+        }
+        
     // Update DOM to reflect changes.
-    var html = "<p>Wins: </p>" + wins + "<p>Losses: </p>" + losses + "<p>Guesses: " + guesses + "</p>";
+    var html = "<p>Guess what letter I'm thinking of...</p>" + "<p>Wins: </p>" + wins + "<p>Losses: </p>" + losses + "<p>Guesses Left: " + guessesLeft + "</p>";
 
     document.querySelector("#game").innerHTML = html;
     }
